a4c39c1f56d5597682944026a5a30ecf8df3637f
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-08-13 20:49:54 +0500

    prepare for deployment

--- next.config.js
+++ next.config.js
@@ -3,4 +3,27 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return{
+      env: {
+        mongodb_username: 'hassaanqaisar2',
+        mongodb_password: 'AmsIRWJATZeuAllj',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'blog'
+      },
+    };
+  }
+
+  return{
+  env: {
+    mongodb_username: 'hassaanqaisar2',
+    mongodb_password: 'AmsIRWJATZeuAllj',
+    mongodb_clustername: 'cluster0',
+    mongodb_database: 'blog_contact'
+  }
+}
+}
