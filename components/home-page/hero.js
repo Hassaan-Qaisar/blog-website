56673fad24ece5cf781b2fc25cce9148e3608b7f
Hassaan-Qaisar <106833785+Hassaan-Qaisar@users.noreply.github.com>
2023-09-24 22:02:44 +0500

    Update hero.js

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,16 +7,16 @@ function Hero() {
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='/images/site/max.png'
+          src='/images/site/max2.jpg'
           alt='An image showing Max'
           width={300}
           height={300}
         />
       </div>
-      <h1>Hi, I'm Max</h1>
+      <h1>Hi, I'm Hassaan</h1>
       <p>
-        I blog about web development - especially frontend frameworks like
-        Angular or React.
+        I blog about web development - especially frameworks like
+        React or Node.
       </p>
     </section>
   );
